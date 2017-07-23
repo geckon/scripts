3f41ba92a05c631c226b3846c63127f535a1cbcc
Tomáš Heger <heger.tomas@gmail.com>
2017-07-23 22:10:48 +0200

    Add cut_video.sh

---
+++ cut_video.sh
@@ -0,0 +1,80 @@
+#! /bin/bash
+#
+# Simple video cutter
+#
+# Copyright (c) 2017 Tomáš Heger
+# Available under the MIT License
+#
+# For help run without arguments.
+#
+
+########################################################################
+# INIT
+########################################################################
+
+SUFFIX="_cut"
+COMMAND="ffmpeg"
+
+########################################################################
+# USAGE
+########################################################################
+
+USAGE="Simple video cutter - usage:
+\n\n
+$0 source_file start length [dest_file]
+\n\n
+dest_file is not required - if it's missing, the result will be named as
+the source_file with $SUFFIX added before the file extension.
+\n\n
+source_file won't be modified.
+\n\n
+EXAMPLES:
+\n\n
+$0 vids/myvideo.mp4 00:00:42 00:00:13 res/cut.mp4 \n
+\t- cut 13 seconds from vids/myvideo.mp4 starting from the 42nd second
+    and store the result as res/cut.mp4
+\n\n
+$0 vids/myvideo2.mp4 00:00:00 00:01:00 \n
+\t- cut the first minute from vids/myvideo2.mp4 and store it as
+    myvideo2_cut.mp4 in the current directory
+\n"
+
+########################################################################
+# CODE
+########################################################################
+
+# If there is wrong number of arguments, print help
+if [[ "$#" -lt 3 ]] || [[ "$#" -gt 4 ]] ; then
+	echo -e $USAGE
+	exit 0
+fi
+
+# Check that the important $COMMAND is installed
+if ! type $COMMAND >/dev/null 2>&1 ; then
+	>&2 echo "ERROR: You need $COMMAND installed to run this."
+	exit 1
+fi
+
+# Determine dest_file
+if [[ "$#" -eq 4 ]] ; then
+	DEST="$4"
+else
+	FILE=`basename "$1"`
+	NAME="${FILE%.*}"
+	if [[ $FILE =~ \. ]] ; then
+		EXT=".${FILE##*.}"
+	else
+		EXT=""
+	fi
+	DEST="$NAME$SUFFIX$EXT"
+fi
+
+# Check that dest_file doesn't already exist
+if [[ -f $DEST ]] || [[ -d $DEST ]] ; then
+	>&2 echo "ERROR: '$DEST' already exists. I won't overwrite it."
+	exit 2
+fi
+
+echo -e "Running\n $COMMAND -ss $2 -i $1 -t $3 -vcodec copy -acodec copy $DEST"
+$COMMAND -ss $2 -i $1 -t $3 -vcodec copy -acodec copy $DEST
+
