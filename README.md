685917b9b15df9075eff1e8b800164fe34f526e6
Tomáš Heger <heger.tomas@gmail.com>
2020-11-23 21:22:00 +0100

    Add a few config options to image_scraper, fix a few issues

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # scripts
-Usually one purpose scripts provided without any warranty under the MIT License. Read the disclaimer at the bottom of this readme.
+Usually one purpose scripts provided without any warranty under the MIT
+License. Read the disclaimer at the bottom of this readme.
 
 [![Updates](https://pyup.io/repos/github/geckon/scripts/shield.svg)](https://pyup.io/repos/github/geckon/scripts/)
 
@@ -7,7 +8,8 @@ Usually one purpose scripts provided without any warranty under the MIT License.
 
 ### Directly runnable
 
-The following scripts are available in `bin` so you can include the directory in your `$PATH` if you are brave or foolish enough:
+The following scripts are available in `bin` so you can include the directory
+in your `$PATH` if you are brave or foolish enough:
 
 ##### `cr2_to_jpg.sh`
 - Converts all CR2 RAW photos in the current directory to JPGs.
@@ -33,13 +35,25 @@ The following scripts are available in `bin` so you can include the directory in
 
 #### Image scraper
 
-This is a very simple and stupid image scraper that will crawl a given website, look for images and download them into specified path on your filesystem.
+This is a very simple and stupid image scraper that will crawl a given website,
+look for links to images and download them into specified path on your
+filesystem. It can probably be used for scraping other files as well but it has
+been written with images in mind.
 
-There is a very important configuration inside `image_scraper/image_scraper.py` - double check that and the source code before running. Only then you may do:
+There is a very important configuration inside `image_scraper/image_scraper.py`.
+Double check that and the source code before running. Only then you may do:
 ```
 $ pip install -r image_scraper/requirements.txt
 $ scrapy runspider image_scraper/image_scraper.py
 ```
 
+DO NOT run if you don't know what you're doing. It has been written and tested
+with one particular website in mind and may very well not work for different
+websites and/or screw something up.
+
 ## ☠ DISCLAIMER ☠
-While there is no malicious code included on purpose and the scripts were written with the best intentions, they may still do harm. Do not run any of the scripts unless you examined and understood what they do and even then be careful with them. The scripts may delete your files, destroy your computer, blow your house up, injure people and even kill some kittens.
+While there is no malicious code included on purpose and the scripts were
+written with the best intentions, they may still do harm. Do not run any of
+the scripts unless you examined and understood what they do and even then be
+careful with them. The scripts may delete your files, destroy your computer,
+blow your house up, injure people and even kill some kittens.
--- image_scraper/image_scraper.py
+++ image_scraper/image_scraper.py
@@ -7,12 +7,13 @@ import urllib
 
 """
 This is a very simple and stupid image scraper that will crawl a given website
-(INITIAL_URL), look for images (files ending with an extension specified in
-IMAGE_EXTENSIONS) according to IMAGE_HREF_XPATH, depending on SUBDIRECTORIES it
-will also look into "subdirectories" or not and store all the images to
-a directory specified by PATH_TO_IMAGE_STORE (all image filenames are updated
-with the image's checksum to avoid downloading the same image twice if it's
-linked twice).
+(INITIAL_URL), look for links to images (files ending with an extension
+specified in IMAGE_EXTENSIONS) according to IMAGE_HREF_XPATH, depending on
+BROWSE_SUBDIRECTORIES it will also look into "subdirectories" or not and store
+all the images to a directory specified by PATH_TO_IMAGE_STORE (all image
+filenames are updated with the image's checksum to avoid downloading the same
+image twice if it's linked twice). It may or may not create subdirectory
+structure based on CREATE_SUBDIRECTORIES.
 
 See Config below.
 
@@ -20,17 +21,22 @@ Run:
 $ scrapy runspider image_scraper.py
 
 No guarantees at all provided. DO NOT run if you don't know what you're doing.
-It may very well screw something up.
+It has been written and tested with one particular website in mind and may very
+well not work for different websites and/or screw something up.
 """
 
 
 # Config:
 #
+# Add checksum to filename?
+ADD_CHECKSUM_TO_FILENAME = False
+#
 # Collect ignored (non considered image) extensions? Does not affect the image
 #  collecting itself.
 COLLECT_IGNORED_EXTENSIONS = False
 #
-# Image extensions to download.
+# Image extensions to download. It should also work for non-image file
+#  extensions.
 IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png']
 #
 # Where to look for image URLs?
@@ -42,10 +48,26 @@ INITIAL_URL = ''
 # Filesystem path to store images to.
 PATH_TO_IMAGE_STORE = ''
 #
-# Browse "SUBdirectories" or not? The logic is very stupid and it will consider
+# Browse "subdirectories" or not? The logic is very stupid and it will consider
 #  any path not containing a dot a directory.
-SUBDIRECTORIES = True
+BROWSE_SUBDIRECTORIES = True
+#
+# Recreate the directory structure and store images into subdirectories as
+#  opposed to storing everything in one flat directory?
+CREATE_SUBDIRECTORIES = True
 
+
+def is_url_absolute(url):
+    """Return True if the given url is absolute, False otherwise."""
+    return bool(urllib.parse.urlparse(url).netloc)
+
+
+def is_url_external(url):
+    """Return True if the url is external comparing to INITIAL_URL."""
+    return (
+        is_url_absolute(url) and
+        not url.startswith(INITIAL_URL)
+    )
 
 class ImageSpider(scrapy.Spider):
     name = 'image_spider'
@@ -62,38 +84,66 @@ class ImageSpider(scrapy.Spider):
                 ignored_exts = response.meta.get('ignored_exts')
 
         for href in response.xpath(IMAGE_HREF_XPATH).getall():
+            # Skip external links.
+            if is_url_external(href):
+                continue
+
             href_split = href.rsplit('.')
             if not href_split:
                 # Empty href?
                 continue
             elif len(href_split) == 1:
-                # This is a subdirectory.
-                if not SUBDIRECTORIES:
+                # This is probably a subdirectory.
+                if not BROWSE_SUBDIRECTORIES:
                     continue
 
-                # Dive into it.
+                # Prepare parameters for recursive runs.
+                meta_params = {}
                 if COLLECT_IGNORED_EXTENSIONS:
-                    yield scrapy.Request(
-                        response.urljoin(href),
-                        self.parse,
-                        meta={'ignored_exts': ignored_exts}
+                    meta_params['ignored_exts'] = ignored_exts
+                if CREATE_SUBDIRECTORIES:
+                    meta_params['relative_path'] = os.path.join(
+                        response.meta.get('relative_path', ''),
+                        urllib.parse.unquote_plus(href)
                     )
-                else:
-                    yield scrapy.Request(response.urljoin(href), self.parse)
+
+                # Dive into it.
+                yield scrapy.Request(
+                    response.urljoin(href),
+                    self.parse,
+                    meta=meta_params
+                )
             elif href_split[1] in IMAGE_EXTENSIONS:
                 # Get the image.
                 image_url = urllib.parse.urljoin(response.url, href)
-                response = requests.get(image_url, stream=True)
-                if not response.ok:
+                image_response = requests.get(image_url, stream=True)
+                if not image_response.ok:
                     continue
 
-                # Compute the checksum and path.
-                image_bytes = response.raw.read()
-                image_hash = hashlib.md5(image_bytes).hexdigest()
-                # Make filename readable better.
-                without_ext = urllib.parse.unquote_plus(href_split[0])
-                filename = f'{without_ext}_{image_hash}.{href_split[1]}'
-                filepath = os.path.join(PATH_TO_IMAGE_STORE, filename)
+                # Compute the checksum and filename.
+                image_bytes = image_response.raw.read()
+                # Make filename readable better, also use basename only.
+                without_ext = urllib.parse.unquote_plus(
+                    os.path.basename(href_split[0])
+                )
+                if ADD_CHECKSUM_TO_FILENAME:
+                    image_hash = hashlib.md5(image_bytes).hexdigest()
+                    filename = f'{without_ext}_{image_hash}.{href_split[1]}'
+                else:
+                    filename = f'{without_ext}.{href_split[1]}'
+
+
+                # Determine filesystem path.
+                if CREATE_SUBDIRECTORIES:
+                    dir_path = os.path.join(
+                        PATH_TO_IMAGE_STORE,
+                        response.meta.get('relative_path', '')
+                    )
+                    if not os.path.exists(dir_path):
+                        os.makedirs(dir_path)
+                else:
+                    dir_path = PATH_TO_IMAGE_STORE
+                filepath = os.path.join(dir_path, filename)
 
                 # Skip we already have this.
                 if os.path.exists(filepath):
